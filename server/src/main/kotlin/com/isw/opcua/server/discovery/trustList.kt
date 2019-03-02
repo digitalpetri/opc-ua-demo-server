0014fa0b00bd0239cf74461bdbda556738cfb259
Kevin Herron <kevin@inductiveautomation.com>
2019-03-02 12:20:48 -0800

    trust list encoding

--- server/src/main/kotlin/com/isw/opcua/server/discovery/trustList.kt
+++ server/src/main/kotlin/com/isw/opcua/server/discovery/trustList.kt
@@ -2,24 +2,15 @@ package com.isw.opcua.server.discovery
 
 import com.isw.opcua.server.objects.TrustListObject
 import com.isw.opcua.server.objects.getTrustListDataType
-import com.isw.opcua.server.util.ExecutableByAdmin
-import io.netty.buffer.ByteBuf
-import io.netty.buffer.Unpooled
 import org.eclipse.milo.opcua.sdk.server.OpcUaServer
-import org.eclipse.milo.opcua.sdk.server.model.methods.*
 import org.eclipse.milo.opcua.sdk.server.model.nodes.objects.TrustListNode
-import org.eclipse.milo.opcua.sdk.server.nodes.UaMethodNode
-import org.eclipse.milo.opcua.stack.core.StatusCodes
-import org.eclipse.milo.opcua.stack.core.UaException
 import org.eclipse.milo.opcua.stack.core.application.DirectoryCertificateValidator
-import org.eclipse.milo.opcua.stack.core.serialization.OpcUaBinaryStreamEncoder
+import org.eclipse.milo.opcua.stack.core.serialization.EncodingLimits
+import org.eclipse.milo.opcua.stack.core.types.OpcUaDataTypeManager
+import org.eclipse.milo.opcua.stack.core.types.OpcUaDefaultBinaryEncoding
 import org.eclipse.milo.opcua.stack.core.types.builtin.ByteString
-import org.eclipse.milo.opcua.stack.core.types.builtin.ExtensionObject
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UByte
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger
-import org.eclipse.milo.opcua.stack.core.types.enumerated.TrustListMasks
+import org.eclipse.milo.opcua.stack.core.types.structured.TrustListDataType
 import java.io.File
-import java.util.concurrent.atomic.AtomicReference
 
 
 fun configureTrustList(server: OpcUaServer, trustListNode: TrustListNode) {
@@ -31,13 +22,14 @@ fun configureTrustList(server: OpcUaServer, trustListNode: TrustListNode) {
             val trustList = validator.getTrustListDataType()
 
             File.createTempFile("TrustListDataType", null).apply {
-                val buffer = Unpooled.buffer()
-                val encoder = OpcUaBinaryStreamEncoder(buffer)
-                encoder.writeExtensionObject(ExtensionObject.encode(trustList))
+                val encoded = OpcUaDefaultBinaryEncoding.getInstance().encode(
+                    trustList,
+                    TrustListDataType.BinaryEncodingId,
+                    EncodingLimits.DEFAULT,
+                    OpcUaDataTypeManager.getInstance()
+                ) as ByteString
 
-                println(path)
-
-                writeBytes(buffer.array())
+                writeBytes(encoded.bytesOrEmpty())
             }
         },
         validator
