d184ad9f6a4f5643c72fecd6562257ea07ab515c
Kevin Herron <kevinherron@gmail.com>
2020-03-11 09:31:18 -0700

    Removed unnecessary copyright headers

--- src/main/kotlin/com/isw/opcua/server/types/CustomEnumType.kt
+++ src/main/kotlin/com/isw/opcua/server/types/CustomEnumType.kt
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2019 the Eclipse Milo Authors
- *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- */
 package com.isw.opcua.server.types
 
 import com.isw.opcua.server.namespaces.demo.DemoNamespace
