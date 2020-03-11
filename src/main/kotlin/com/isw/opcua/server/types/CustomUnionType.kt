518ecbc8fbaaf2b84715eeaf8b6fae5b3a2e8e5e
Kevin Herron <kevinherron@gmail.com>
2020-03-11 09:30:43 -0700

    equals/hashCode/toString for CustomUnionType

--- src/main/kotlin/com/isw/opcua/server/types/CustomUnionType.kt
+++ src/main/kotlin/com/isw/opcua/server/types/CustomUnionType.kt
@@ -9,6 +9,7 @@
  */
 package com.isw.opcua.server.types
 
+import com.google.common.base.MoreObjects
 import com.isw.opcua.server.namespaces.demo.DemoNamespace
 import org.eclipse.milo.opcua.stack.core.StatusCodes
 import org.eclipse.milo.opcua.stack.core.UaSerializationException
@@ -53,6 +54,33 @@ class CustomUnionType private constructor(private val type: Type, private val va
 
     val isBar: Boolean
         get() = type == Type.Bar
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as CustomUnionType
+
+        if (type != other.type) return false
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + type.hashCode()
+        result = 31 * result + (value?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return MoreObjects.toStringHelper(this)
+            .add("type", type)
+            .add("value", value)
+            .toString()
+    }
 
     internal enum class Type {
         Null, Foo, Bar
