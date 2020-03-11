29786244f259332fa61464ab56621b0ee0ddc259
Kevin Herron <kevinherron@gmail.com>
2020-03-11 09:24:21 -0700

    Inspection fixup

--- src/main/kotlin/com/isw/opcua/server/types/CustomStructType.kt
+++ src/main/kotlin/com/isw/opcua/server/types/CustomStructType.kt
@@ -40,10 +40,10 @@ class CustomStructType @JvmOverloads constructor(
         return ExpandedNodeId.NULL_VALUE
     }
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as CustomStructType
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val that = other as CustomStructType
         return isBaz == that.isBaz &&
             Objects.equal(foo, that.foo) &&
             Objects.equal(bar, that.bar)
